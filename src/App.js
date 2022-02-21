a2488e80d13c728978c5f1cc7c14ce7bef715592
이도현 <ksmfou98@naver.com>
2022-02-21 20:31:33 +0900

    feat: ImageBanner 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/index.js";
 import IFrameBox from "./components/IFrameBox/index.js";
+import ImageBanner from "./components/ImageBanner/index.js";
 import ImageSlider from "./components/ImageSlider/index.js";
 
 export default class App {
@@ -7,5 +8,6 @@ export default class App {
     new Header($app);
     new IFrameBox($app);
     new ImageSlider($app);
+    new ImageBanner($app);
   }
 }
