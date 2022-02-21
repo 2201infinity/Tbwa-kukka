564cc7af361c34891d0c26dc4400ab210e78cb11
이도현 <ksmfou98@naver.com>
2022-02-21 20:18:08 +0900

    feat: layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from "./components/Header/index.js";
+import IFrameBox from "./components/IFrameBox/index.js";
 
 export default class App {
   constructor($app) {
     new Header($app);
+    new IFrameBox($app);
   }
 }
