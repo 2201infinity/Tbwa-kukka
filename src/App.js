1e590cdb223a4eabdc2bff49cebfa6588662f159
yezyvibe <yeji0501@gmail.com>
2022-02-21 23:50:07 +0900

    feat: 헤더 네브바 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ImageSlider from "./components/ImageSlider/index.js";
 export default class App {
   constructor($app) {
     new Header($app);
-    new IFrameBox($app);
+    // new IFrameBox($app);
     new ImageSlider($app);
     new ImageBanner($app);
     new Footer($app);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,12 +9,50 @@ export default class Header {
   }
 
   render() {
+    const menus = [
+      "꽃 정기구독",
+      "꽃다발",
+      "당일배송",
+      "플라워클래스",
+      "소품샵",
+      "이벤트",
+    ];
     this.$container.innerHTML = ``;
 
     const $innerHeader = document.createElement("div");
     $innerHeader.className = "inner-header";
-    $innerHeader.innerText = "난 헤더야";
+    // $innerHeader.innerText = "난 헤더야";
 
+    const $menuLogo = document.createElement("a");
+    $menuLogo.className = "menu-logo";
+    $menuLogo.href = "/";
+    $menuLogo.innerHTML = "KUKKA";
+
+    const $menuList = document.createElement("ul");
+    $menuList.className = "menu-list";
+    // $menuList.innerHTML = "난 리스트";
+
+    menus.map((menu) => {
+      const $menuItem = document.createElement("div");
+      $menuItem.className = "menu-item";
+      $menuItem.innerHTML = `
+        <li>
+        ${menu}
+        </li>
+      `;
+      $menuList.appendChild($menuItem);
+    });
+
+    const $userMenu = document.createElement("ul");
+    $userMenu.className = "user-menu";
+    $userMenu.innerHTML = `
+      <li>유저</li>
+      <li>장바구니</li>
+    `;
+
+    $innerHeader.appendChild($menuLogo);
+    $innerHeader.appendChild($menuList);
+    $innerHeader.appendChild($userMenu);
     this.$container.appendChild($innerHeader);
   }
 }
