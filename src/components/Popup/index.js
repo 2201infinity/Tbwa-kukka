0b935c4a5951eaf30ea9bbe093078d3b9295d943
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-22 10:41:48 +0900

    refactor: 팝업 이미지 상수화

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,3 +1,4 @@
+import { POPUP_CONTENT_IMAGE } from "../../utils/constants.js";
 import "./styles.css";
 
 export default class Popup {
@@ -23,8 +24,7 @@ export default class Popup {
 
     const $PopupImage = document.createElement("img");
     $PopupImage.className = "PopupImage";
-    $PopupImage.src =
-      "https://kukka-2-media-123.s3.amazonaws.com/media/class-name/ContentsPopups/11/20220221_%EB%B0%B0%EC%86%A1%EC%95%88%EB%82%B4_G6jZAMi.png";
+    $PopupImage.src = POPUP_CONTENT_IMAGE;
     $PopupImage.alt = "Popup Image";
     $PopupImage.style.width = "100%";
 
